093cf10ccb72f05591730a32752b30ec01db196a
Albert Fotso <legrandpone1@gmail.com>
2025-03-29 21:36:19 +0100

    Frontend initialiser ok

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import {Button} from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      FindIt - Plateforme de retrouvailles d'objets
+      <div className="text-2xl">En construction </div>
+      <Button>
+  
+          <span>Partager</span>
+     
+      </Button>
+    </div>
+  )
+}
+export default App
